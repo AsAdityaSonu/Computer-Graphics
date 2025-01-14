01e55420e449c9fc1b6a9e0cb3a4e683920fef8c
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2025-01-14 16:50:45 +0530

    Initial Commit

---
+++ Computer-Graphics/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Computer-Graphics
+//
+//  Created by Aditya Pandey on 14/01/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
